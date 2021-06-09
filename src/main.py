a286a7d1ec8dea8f13e310f4c008ff25ea24df2c
jatv93 <toledojennifer20@gmail.com>
2021-06-09 19:55:09 +0000

    database

--- src/main.py
+++ src/main.py
@@ -8,17 +8,21 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Payment, Invoice
 #from models import Person
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, "test.db")
+app.config["DEBUG"] =True
+app.config["ENV"] = "development"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -31,13 +35,90 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user/<int:id>', methods=['GET'])
+def users(id = None):
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    if request.method == 'GET':
+        if id is not None:
+            user = User.query.get(id) 
+            if user:
+                return jsonify(user.serialize()), 200
+            return jsonify({"msg": "User not found"}), 404
+        else:
+            user = User.query.all()
+            user = list(map(lambda user: user.serialize(), user))
+            return jsonify(user), 200
 
-    return jsonify(response_body), 200
+
+@app.route('/payment', methods=['GET'])
+@app.route('/payment/<int:id>', methods=['GET', 'POST'])
+def payments(id = None):
+
+    if request.method == 'GET':
+        if id is not None:
+            payment = Payment.query.get(id) 
+            if payment:
+                return jsonify(payment.serialize()), 200
+            return jsonify({"msg": "Payment not found"}), 404
+        else:
+            payment = Payment.query.all()
+            payment = list(map(lambda payment: payment.serialize(), payment))
+            return jsonify(payment), 200
+
+    if request.method == 'POST':
+
+        amount = request.json.get("amount", None)
+        receipt_name = request.json.get("receipt_name", None)
+        subject = request.json.get("subject", None)
+        payment_file = request.json.get("payment_file", None)
+
+        if not amount:
+            return jsonify({"msg": "Amount is required"}), 400
+        if not receipt_name:
+            return jsonify({"msg": "Name is required"}), 400
+        if not subject:
+            return jsonify({"msg": "Subject is required"}), 400
+        if not payment_file:
+            return jsonify({"msg": "Payment File is required"}), 400
+        
+        payment = Payment()
+        payment.amount = amount
+        payment.receipt_name = receipt_name
+        payment.subject = subject
+        payment.payment_file = payment_file
+
+        payment.save()
+
+        return jsonify({"success": "Payment Register Successfully"}), 200
+
+@app.route('/invoice', methods=['GET'])
+@app.route('/invoice/<int:id>', methods=['GET', 'POST'])
+def invoices(id = None):
+
+    if request.method == 'GET':
+        if id is not None:
+            invoice = Invoice.query.get(id) 
+            if invoice:
+                return jsonify(invoice.serialize()), 200
+            return jsonify({"msg": "Invoice not found"}), 404
+        else:
+            invoice = Invoice.query.all()
+            invoice = list(map(lambda invoice: invoice.serialize(), invoice))
+            return jsonify(invoice), 200
+
+    if request.method == 'POST':
+
+        files = request.json.get("files", None)
+
+        if not files:
+            return jsonify({"msg": "File is required"}), 400
+        
+        invoice = Invoice()
+        invoice.files = files
+
+        invoice.save()
+
+        return jsonify({"success": "Invoice Register Successfully"}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    payments = db.relationship('Payment', backref='user', lazy=True)
+    invoices = db.relationship('Invoice', backref='user', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name
         }
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String, db.ForeignKey('user.email'), nullable=False)
+    amount = db.Column(db.Float(8), unique=False, nullable=False)
+    receipt_name = db.Column(db.String(120), unique=False, nullable=False)
+    subject = db.Column(db.String(120), unique=False, nullable=False)
+    payment_file = db.Column(db.String(120), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "amount": self.amount,
+            "receipt_name": self.receipt_name,
+            "subject": self.subject,
+            "payment_file": self.payment_file,
+            "email": self.email.serialize()
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Invoice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String, db.ForeignKey('user.email'), nullable=False)
+    files = db.Column(db.String(120), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "files": self.files,
+            "email": self.email.serialize()
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
